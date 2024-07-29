548a728a88fb930473215fded3b6fea257a62d02
Sheraz Arif <sherazarifofficial@gmail.com>
2024-07-29 14:34:03 +0500

    added footer

--- src/app/_components/Footer/Footer.jsx
+++ src/app/_components/Footer/Footer.jsx
@@ -5,13 +5,33 @@ import { assets } from "../../../../public/assets";
 const Footer = () => {
   return (
     <div className="bg-black py-6 px-5">
-      <Image
-        src={assets.logo_light}
-        width={180}
-        height={180}
-        alt="image"
-        className="m-auto "
-      />
+      <div className=" flex justify-around items-center">
+        <Image
+          src={assets.logo_light}
+          width={180}
+          height={180}
+          alt="image"
+          className=""
+        />
+        <div className="flex gap-3">
+          <Image
+            src={assets.facebook_icon}
+            width={40}
+            height={40}
+            alt="image"
+          />
+          <Image src={assets.twitter_icon} width={40} height={40} alt="image" />
+          <Image
+            src={assets.googleplus_icon}
+            width={40}
+            height={40}
+            alt="image"
+          />
+        </div>
+      </div>
+      <div className="text-white text-center mt-8 text-lg">
+        All rights reserved. @blogger
+      </div>
     </div>
   );
 };
