74daf0409b07e0af3b0e6212477f0d5291a79527
Sheraz Arif <sherazarifofficial@gmail.com>
2024-07-29 17:38:31 +0500

    adding detail page

--- src/app/blogs/[id]/page.jsx
+++ src/app/blogs/[id]/page.jsx
@@ -2,6 +2,7 @@
 
 import React, { useEffect, useState } from "react";
 import { blog_data } from "../../../../public/assets";
+import Image from "next/image";
 
 const page = ({ params }) => {
   const [data, setData] = useState(null);
@@ -17,7 +18,38 @@ const page = ({ params }) => {
   };
 
   useEffect(() => FetchBlogData(), []);
-  return <div>{params.id}</div>;
+  return data ? (
+    <div>
+      <div className="py-16 bg-slate-300 text-center">
+        <h1 className="md:text-4xl text-2xl font-bold max-w-[800px] m-auto">
+          {data.title}
+        </h1>
+        <Image
+          src={data.author_img}
+          width={80}
+          height={80}
+          alt="image"
+          className="m-auto mt-8"
+        />
+      </div>
+      <div className="my-10 lg:px-16 md:px-8 px-4 ">
+        <div className="relative aspect-video max-w-3xl  m-auto mt-8 overflow-hidden ">
+          <Image
+            src={data.image}
+            fill
+            alt="image"
+            className="w-full h-full object-cover hover:scale-110 duration-500 "
+          />
+        </div>
+        <div className="max-w-[770px] m-auto">
+          <p className="text-2xl font-bold">Introduction</p>
+          <p>{data.description}</p>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <></>
+  );
 };
 
 export default page;
